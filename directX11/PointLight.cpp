fc8cd70845bdf6400c3e858437fd48fe40a2d8eb
kurishiro <ug41746@gmail.com>
2020-08-12 15:30:29 +0900

    ライトコントローラー

--- directX11/PointLight.cpp
+++ directX11/PointLight.cpp
@@ -24,6 +24,18 @@ void CPointLight::SpawnControlWindow() noexcept
 		ImGui::SliderFloat("X", &m_cbData.pos.x, -60.0f, 60.0f, "%.1f");
 		ImGui::SliderFloat("Y", &m_cbData.pos.y, -60.0f, 60.0f, "%.1f");
 		ImGui::SliderFloat("Z", &m_cbData.pos.z, -60.0f, 60.0f, "%.1f");
+		// ã≠Ç≥ êF
+		ImGui::Text("Intensity/Color");
+		ImGui::SliderFloat("Intensity", &m_cbData.diffuseIntensity, 0.01f, 2.0f, "%.2f", 2);
+		ImGui::ColorEdit3("Diffuse Color", &m_cbData.diffuseColor.x);
+		ImGui::ColorEdit3("Ambient", &m_cbData.ambient.x);
+		ImGui::ColorEdit3("Material", &m_cbData.material.x);
+		// å∏êä
+		ImGui::Text("Falloff");
+		ImGui::SliderFloat("Constant", &m_cbData.attConst, 0.05f, 10.0f, "%.2f", 4);
+		ImGui::SliderFloat("Linear", &m_cbData.attLin, 0.0001f, 4.0f, "%.4f", 8);
+		ImGui::SliderFloat("Quadratic", &m_cbData.attQuad, 0.0000001, 10.0f, "%.7f", 10);
+	
 		if (ImGui::Button("Reset"))
 		{
 			Reset();
